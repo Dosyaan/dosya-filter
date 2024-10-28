4b09f8c04bb9195bb1c343e1e3af4a2ca4c9aaae
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-28 17:55:47 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+// task - 1
+// const num = [1,2,3,4,5,6]
+// const filterArr = num.filter((item) =>{
+//         return item.num %2==0;
+// })
+// console.log(filterArr)
+
+
+// task - 2
+
+// const animals = ['кот', 'собака', "тигр", "лев"]
+
+// const filterArr = animals.filter((item) =>{
+//     if (item.length>3){
+//         return true
+//     }
+// })
+
+// console.log(filterArr)
+
+// task - 3
+
+// const num = [5, 12, 8, 20, 3]
+// const filterArr = num.filter((item) =>{
+//     if (item > 10){
+//         return true
+//     }
+// })
+
+// console.log(filterArr)
+
+// task - 4
+
+// const fruits = ['апельсин', 'яблоко', 'арбуз', 'банан']
+// const filterArr = fruits.filter((item) => {
+//     return item.toLowerCase().startsWith('а')
+// })
+
+// console.log(filterArr)
+
+// task - 5
+
+// const users = [
+//     { name: 'Дастан', age: 20 },
+//     { name: 'Руслан', age: 17 },
+//     { name: 'Ариан', age: 25 },
+//     { name: 'Эмир', age: 16 },
+//     { name: 'Чингиз', age: 19 }
+// ]
+
+// const young = users.filter((item) => {
+//         return item.age > 18
+// })
+
+// console.log(young)
